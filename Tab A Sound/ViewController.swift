d87897d0a64f0a2cf8bbc5b018011c8f837ce1ed
Kuei <vx3012564897@gmail.com>
2020-03-18 00:20:00 +0800

    already finished project

--- Tab A Sound/ViewController.swift
+++ Tab A Sound/ViewController.swift
@@ -7,9 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer : AVAudioPlayer?
 
+    let soundArr = ["Sound1","Sound2","Sound3","Sound4","Sound5"]
+    
+    //var soundFileName : String = ""
+  
+    @IBAction func buttonPress(_ sender: UIButton) {
+//        print(sender.tag)
+        
+        let soundFileName : String = soundArr[sender.tag - 1]
+        
+        
+        playSound(fileName:soundFileName)
+        
+    }
+
+    func playSound(fileName : String){
+        let url = Bundle.main.url(forResource: "Sound/\(fileName)", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+        }catch {
+            print("Play error!")
+        }
+        
+        audioPlayer?.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
